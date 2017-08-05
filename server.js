75a8a70d69c8508ac30cf7e89b9042ce63b6c86d
M-anjana23 <anjanameena98@yahoo.com>
2017-08-05 23:35:54 +0545

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 app.get('/article-one', function (req, res) {
   res.sendFile('from 1');
 });
@@ -21,6 +18,10 @@ app.get('/article-two', function (req, res) {
 app.get('/article-three', function (req, res) {
   res.sendFile('from 3');
 });
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
